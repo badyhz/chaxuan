0f6951007f39007df9db0629907c68600d6774af
badyhz <badyhz@qq.com>
2025-12-04 14:23:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // 提供静态文件服务
 app.use(express.static(path.join(__dirname)));
